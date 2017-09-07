a266b2eba73a5f6d2f3450e2e1f31fea2d0d227e
Chad Garrett <chadgarrett_@hotmail.com>
2017-09-07 20:50:14 +0000

    Completed makeLoadedDie() function and added ability for the die to start at beginning of list if the end is reached

--- makeLoadedDie.js
+++ makeLoadedDie.js
@@ -1,9 +1,18 @@
+/*
+
+    Create a function called makeLoadedDie() which returns a function that when called, seems to generate random numbers between 1 and 6, but in fact returns numbers in a row from a hardcoded list.
+
+*/
+
 function makeLoadedDie() {
     var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-    /* your code here */
+    var currIndex = 0;
 
     return function() {
-        /* your code here */
+        if (currIndex > (list.length-1)) {
+            currIndex = 0;
+        }
+        return list[currIndex++];
     }
 }
 
